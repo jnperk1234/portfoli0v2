1229034ea8b08c6c648614f43bd460a555f18da6
Jessica Perkins <jnperk1234@gmail.com>
2019-06-19 21:20:09 -0500

    upd5

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,22 +49,22 @@ $('.main_nav li a').click(function() {
 /* Smooth Scrolling
 ------------------------------------------------------ */
 
-jQuery(document).ready(function($) {
+// jQuery(document).ready(function($) {
 
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
+//    $('.smoothscroll').on('click',function (e) {
+// 	    e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
+// 	    var target = this.hash,
+// 	    $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-  
-});
+// 	    $('html, body').stop().animate({
+// 	        'scrollTop': $target.offset().top
+// 	    }, 800, 'swing', function () {
+// 	        window.location.hash = target;
+// 	    });
+// 	});
+
+// });
 
 
-TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
+// TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
